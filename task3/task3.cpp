1e51603c34dbb6bc9caf8ab4862fe20d5d60b4c0
Slava Abramov <slavaabramov2019@mail.ru>
2020-03-11 01:57:31 +0900

    Task 3

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -1,21 +1,29 @@
-/*
-Íàéäèòå ïåðâûé èç ýëåìåíòîâ ìàññèâà, íàèáîëåå îòëè÷àþùèéñÿ îò ñðåäíåãî
-*/
-#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main(){
-	int n, i, maxdiff;
-	double sum, *X;
-	scanf("%d", &n);
+int main() {
+	int n, i;
+	double sum, * X, r, c, max;
+	cin >> n;
 	X = new double[n];
-	for (i=0; i<n; i++){
-		scanf(" %d", &X[i]);
-	
-	
-	}
-	// ...
 
-	printf("%f", X[maxdiff]);
-	delete[] X;
-	return 0;
+	for (i = 0; i < n; i++) {
+		cin >> X[i];
+	}
+	sum = 0;
+	for (i = 0; i < n; i++) {
+		sum = sum + X[i];
+	}
+
+	r = sum / n;
+	max = 0;
+	for (i = 0; i < n; i++) {
+		if (abs(r - X[i]) > max) {
+			max = abs(r - X[i]);
+			c = X[i];
+
+		}
+	}
+	cout << c;
 }
