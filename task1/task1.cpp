b76f41ee7fe0ba61c463cdc4987d3c56320f05a6
NEFUstudent <study.svfu@gmail.com>
2020-03-10 15:32:49 +0900

    Task1,2

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -1,10 +1,24 @@
-// ñêîëüêî áîëüøèõ ïîñëåäíåãî?
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdlib>
+using namespace std;
 
 int main(){
-	int n, m=0, *A;
+	int i, k, s, n;
+	int *A;
+	cin >> n;
+	A = new int[n];
+	for (i = 0; i < n; i++){
+		cin >> A[i];
+	}
+	k = 0;
+	s = A[n-1];
+	for(i = 0; i < n; i++){
+		if (A[i] > s){
+			k = k + 1;
+		}
+	}
+	cout << k;
 
-	// scanf("%d, &n); èëè cin >> n;
-	A = new int [n];
-
-	//...
 }
--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,28 +1,40 @@
-/*
-Ïîìåíÿòü ìåñòàìè ïåðâûé îòðèöàòåëüíûé ñ ïîñëåäíèì îòðèöàòåëüíûì
-*/
-
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
-	int n, *A, i, first, last;
+	int n, *A, i, k, kf, kl, Af, Al;
+
     cin >> n;
 	A = new int[n];
 
 	for (i=0; i<n; i++){
 		cin >> A[i];
-		if (A[i]<0) { //...
-			//...
-		}
-
 	}
-	A[first] = A[last]; 
-	A[last] = A[first]; // òàê äà ??
-	
+	for (i=0; i<n; i++){
+		if (A[i]<0){
+			Af = A[i];
+			kf = i;
+			break;
+		}
+	}
+	for (i=0; i<n; i++){
+		if (A[i]<0){
+			Al = A[i];
+			kl = i;
+		}
+	}
+	for (i=0; i<n; i++){
+		if (i = kl) {
+			A[i] = Af;
+		}
+		if (i = kf) {
+			A[i] = Al;
+		}
+		break;
 	for (i=0; i<n; i++){
 		cout << A[i];
 	}
-	return 0;
 }	
+}
