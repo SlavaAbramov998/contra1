a5bf2001cfdb48c4dd9f581c3413cfc4f9b6f2bf
Slava Abramov <slavaabramov2019@mail.ru>
2020-03-12 00:08:49 +0900

    Task 5

--- task5/task5.cpp
+++ task5/task5.cpp
@@ -1,12 +1,44 @@
-/*
-ïåðåñòàâèòü äâå ÷àñòè ìàññèâà
-*/
+#include <iostream>
+using namespace std;
 
-int main(){
-	int n, *A, i;
-	
-	//...
+int main() {
+	int x, y, k, l, n, h, m;
+	double* A;
+	cin >> n;
+	A = new double[n];
+	for (int i = 1; i <= n; i++) {
+		cin >> A[i];
+	}
+	cin >> m;
+
+	l = 1;
+	k = 1;
+	h = n - m;
+
+	if (h == n / 2) {
+		for (int i = 1; i <= h; i++) {
+			y = A[i];
+			x = A[m + k];
+			A[i] = x;
+			A[m + k] = y;
+			k++;
+		}
+	}
+	else {
+		for (int i = m; i < n; i++) {
+			x = A[i + 1];
+			for (int j = m + l; j--;) {
+				if (j == l) break;
+				y = A[j - 1];
+				A[j] = y;
+			}
+			A[l] = x;
+			l++;
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		cout << A[i];
+	}
 
 
-	return 0;
 }
