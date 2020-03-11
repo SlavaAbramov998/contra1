3405f993ac41a0d8d61ec5d198eeef4a627711a4
Slava Abramov <slavaabramov2019@mail.ru>
2020-03-12 00:20:14 +0900

    Task 6

--- task6/task6.cpp
+++ task6/task6.cpp
@@ -1,4 +1,19 @@
-/*  поиск делением пополам */
-int main(){
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+	int n, y, *A, i;
+	cin >> n;
+	A = new int[n];
+	for (i = 0; i < n; i++) {
+		cin >> A[i];
+	}
 
+	cin >> y;
+	for (i = 0; i < n; i++) {
+		if (A[i] > y || A[i] == y) {
+			cout << i;
+			break;
+		}
+	}
 }
