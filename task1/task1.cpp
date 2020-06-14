db56d8de397e6c880b0a7e691de3fc8dc547eac2
SlavaAbramov998 <54822660+SlavaAbramov998@users.noreply.github.com>
2020-06-14 22:55:13 +0900

    Update task1.cpp

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -1,24 +1,12 @@
-
 #include <iostream>
-#include <cstdlib>
-#include <cstdlib>
+
 using namespace std;
 
-int main(){
-	int i, k, s, n;
-	int *A;
-	cin >> n;
-	A = new int[n];
-	for (i = 0; i < n; i++){
-		cin >> A[i];
-	}
-	k = 0;
-	s = A[n-1];
-	for(i = 0; i < n; i++){
-		if (A[i] > s){
-			k = k + 1;
-		}
-	}
-	cout << k;
-
+int main() {
+	float a, b;
+	cin >> a >> b;
+	if (a < 0)
+		cout << "x > " << a / b;
+	if (a > 0)
+		cout << "x < " << a / b;
 }
