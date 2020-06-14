8572dfd918d9178aaec4e7d6b9ee0e799bf52b7b
SlavaAbramov998 <54822660+SlavaAbramov998@users.noreply.github.com>
2020-06-14 22:56:51 +0900

    Update task3.cpp

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
-#include <cstdlib>
+
 using namespace std;
 
 int main() {
-	int n, i;
-	double sum, * X, r, c, max;
+	int* a, i, max = 0, n, k = 0;
 	cin >> n;
-	X = new double[n];
-
-	for (i = 0; i < n; i++) {
-		cin >> X[i];
-	}
-	sum = 0;
-	for (i = 0; i < n; i++) {
-		sum = sum + X[i];
-	}
-
-	r = sum / n;
-	max = 0;
-	for (i = 0; i < n; i++) {
-		if (abs(r - X[i]) > max) {
-			max = abs(r - X[i]);
-			c = X[i];
-
+	a = new int[n];
+	for (i = 0; i < n; i++)
+		cin >> a[i];
+	for (i = 1; i < n; i++)
+		if (a[i] < a[i - 1])
+			k++;
+		else {
+			if (max < k)
+				max = k;
+			k = 0;
 		}
-	}
-	cout << c;
+	if (k > max)
+		cout << k + 1;
+	else
+		if (max == 0)
+			cout << "0";
+		else
+			cout << max + 1;
 }
